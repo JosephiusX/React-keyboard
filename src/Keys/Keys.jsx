46cb0e6ea6e2b2817a649ee29920e5f2f6b8b9ac
josephius x <josephiusx@gmail.com>
2022-11-14 01:32:37 -0800

    concating key inputs with useState

--- src/Keys/Keys.jsx
+++ src/Keys/Keys.jsx
@@ -3,10 +3,13 @@ import React, {useState, useRef, useEffect} from 'react';
 export const Keys = () => {
 
   const keyRef = useRef()
+  
+  const [text, setText] = useState("");
 
-  // function test () {
-  //   console.log(keyRef.current.innerHTML);
-  // }
+  function input (e, text) {
+    e.preventDefault();
+    setText(prevText => prevText + e.target.innerText)
+  }
 
   const [layout, setLayout] = useState([
     "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
@@ -15,24 +18,19 @@ export const Keys = () => {
     "done", "z", "x", "c", "v", "b", "n", "m", ",", ".","?",
     "space"
   ])
-
+  
   const [keys, setkeys] = useState ([
     layout.map((key,index)=>
-      (
-        <button ref={keyRef} onClick={(e) => {
-          e.preventDefault();
-          console.log(e.target.innerHTML)
-        } } key={index}>{key}</button>
-      )
+      
+        <button ref={keyRef} onClick={input} key={index}>{key}</button>
+      
     )
   ])
 
-
-  
-  
-  return(
-    
-     [keys]
-    
+  return (
+    <div>
+      <textarea type="text" autoFocus value={text} onChange={input} />
+      {[keys]}
+    </div>
   )
-}; 
+};
