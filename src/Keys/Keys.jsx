26c7b579b993cb1aa0768af7e9e800ae058a0509
josephius x <josephiusx@gmail.com>
2022-11-16 13:42:16 -0800

    caps lock is toggleing according to the log

--- src/Keys/Keys.jsx
+++ src/Keys/Keys.jsx
@@ -2,13 +2,10 @@ import React, {useState, useRef, useEffect} from 'react';
 
 export const Keys = () => {
   
+  // textarea initial state
   const [text, setText] = useState("");
 
-  function input (e) {
-    e.preventDefault();
-    setText(prevText => prevText + e.target.textContent)
-  }
-
+  // keyboard template state
   const [layout, setLayout] = useState([
     "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
     "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
@@ -16,32 +13,41 @@ export const Keys = () => {
     "done", "z", "x", "c", "v", "b", "n", "m", ",", ".","?",
     "space"
   ])
-  
-  const [capsLock, setCapsLock] = useState(() => {
+
+  const input = (e) => {
+    e.preventDefault();
+    setText(prevText => prevText + e.target.textContent)
+  }
+
+  const initCapsState = () => {
     console.log('setting caps state');
     return false
-  })
+  }
 
-  // turnary operator
-// <if this is true> ? <render this> : <otherwise default to this>
-
+  // capsLock state
+  const [capsLock, setCapsLock] = useState(initCapsState())
+  
   const toggleCaps = () => {
     setCapsLock(capsLock => !capsLock)
-    console.log(keys)
+    console.log(capsLock)
   }
- 
-  const [keys, setkeys] = useState([
-      layout.map((key,index)=> {
 
-        if(key === 'caps') {
-          return <button onClick={toggleCaps} key={index}>{capsLock ? key.toUpperCase() : key.toLowerCase()}</button>
-        } else {
-          return <button onClick={input} key={index}>{capsLock ? key.toUpperCase() : key.toLowerCase()}</button>
-        }
+  // keyboard logic state
+  const [keys, setkeys] = useState([
+    layout.map((key,index)=> {
+      
+      if(key === 'caps') {
+        return <button onClick={toggleCaps} key={index}>{capsLock ? key.toUpperCase() : key.toLowerCase()}</button>
+      } else {
+        return <button onClick={input} key={index}>{capsLock ? key.toUpperCase() : key.toLowerCase()}</button>
       }
+    }
     )
   ])
 
+  useEffect(() => {
+    console.log('capsLock is', capsLock);
+  }, [capsLock, keys])
 
   return (
     <div>
