a826ed565a89172af84366f70fe34ab7b8036455
josephius x <josephiusx@gmail.com>
2022-11-18 18:52:50 -0800

    button logic setup except for enter

--- src/Keys/Keys.jsx
+++ src/Keys/Keys.jsx
@@ -11,16 +11,19 @@ export const Keys = () => {
     "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
     "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
     "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-    "done", "z", "x", "c", "v", "b", "n", "m", ",", ".","?",
+    "clear", "z", "x", "c", "v", "b", "n", "m", ",", ".","?",
     "space"
   ]
 
   // capsLock state
-  const [capsLock, setCapsLock] = useState(false)
+  const [capsLock, setCapsLock] = useState(()=>false)
 
   const toggleCaps = () => setCapsLock(capsLock => !capsLock)
   const space = () => setText(text => text + " ");
   const backspace = () => setText(text => text.slice(0, -1));
+  // logic pending
+  const enter = () => console.log('submit whats in the text area and clear it')
+  const clear = () => setText("");
   
 
   const keysLogic = () => {
@@ -28,8 +31,8 @@ export const Keys = () => {
         if(key === 'caps') return <button onClick={toggleCaps} key={index}>{key}</button>
         if(key === 'space') return <button onClick={space} key={index}>{key}</button>
         if(key === 'backspace') return <button onClick={backspace} key={index}>{key}</button>
-        
-
+        if(key === 'enter') return <button onClick={enter} key={index}>{key}</button>
+        if(key === 'clear') return <button onClick={clear} key={index}>{key}</button>
         else {
           return <button onClick={input} key={index}>{capsLock ? key.toUpperCase() : key.toLowerCase()}</button>
         }
